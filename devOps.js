dc676d180ce482c9e7ca5c891b8cf44eacabca08
Jairo Morales <jairomoralesdlto@hotmail.com>
2023-11-23 13:02:05 -0500

    OKEY

---
+++ devOps.js
@@ -0,0 +1,50 @@
+HOTMAIL
+jairomoralesdlto@hotmail.com
+git remote show origin 
+git remote rm origin 
+
+
+GITHUB
+https://github.com/jairomoralest
+
+U:jairomoralest
+p:Jairo2023..
+
+
+git remote add origin git@github.com:jairomoralest/codigo.git
+
+
+git branch -M main
+git remote add origin https://github.com/jairomoralest/codigo.git
+git push -u origin main
+
+git push -f origin main
+
+
+TOKEN
+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
+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
+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
+
+
+echo "# ejemplo21_NOV" >> README.md
+  git init
+  git add README.md
+  git commit -m "first commit"
+  git branch -M main
+  git remote add origin https://github.com/jairomoralest/ejemplo21_NOV.git
+  git push -u origin main
+…or push an existing repository from the command line
+git remote add origin https://github.com/jairomoralest/ejemplo21_NOV.git
+  git branch -M main
+  git push -u origin main
+
+  fdfd
+fdf
+
+git init
+git add .
+git commit -m "first commit"
+git remote add origin **<YOUR URL>**
+git pull --rebase origin master
+git push -f origin master
